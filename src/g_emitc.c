c049f56f3e5f1fbb4016ad1b08c788824917f0f7
Jang Uk (David) Park <jupark814@gmail.com>
2025-02-20 16:18:21 -0800

    MAC1

--- src/g_emitc.c
+++ src/g_emitc.c
@@ -239,34 +239,34 @@ inst_mul1(const struct g__ann_program_inst *inst, FILE *file)
 			precision(inst),
 			UL(inst->arg[2].i)) ||
 			P(file,
-			"	 int size_A = %lu * %lu * sizeof(float);\n"
-			"	 int size_B = %lu * sizeof(float);\n"
-			"	 int size_z = %lu * sizeof(float);\n"
+			"    int size_A = %lu * %lu * sizeof(float);\n"
+			"    int size_B = %lu * sizeof(float);\n"
+			"    int size_z = %lu * sizeof(float);\n"
+			"    float *deviceB;\n"
 			"    float *deviceA;\n"
-			"	 float *deviceB;\n"
-			"	 float *deviceOutput;\n"
-			"	 cudaMalloc((void**) &deviceA, size_A);\n"
-			"	 cudaMalloc((void**) &deviceB, size_B);\n"
-			"	 cudaMalloc((void**) &deviceOutput, size_z);\n"
-			"	 cudaMemcpy(deviceA, hostA, size_A, cudaMemcpyHostToDevice);\n"
-			"	 cudaMemcpy(deviceB, hostB, size_B, cudaMemcpyHostToDevice);\n",
+			"    float *deviceOutput;\n"
+			"    cudaMalloc((void**) &deviceA, size_A);\n"
+			"    cudaMalloc((void**) &deviceB, size_B);\n"
+			"    cudaMalloc((void**) &deviceOutput, size_z);\n"
+			"    cudaMemcpy(deviceA, hostA, size_A, cudaMemcpyHostToDevice);\n"
+			"    cudaMemcpy(deviceB, hostB, size_B, cudaMemcpyHostToDevice);\n",
 			UL(inst->arg[3].i),
 			UL(inst->arg[4].i),
 			UL(inst->arg[4].i),
 			UL(inst->arg[3].i)) ||
 			P(file,
-				"	 dim3 DimGrid((%lu+255)/256,1,1);\n"
-				"	 dim3 DimBlock(256, 1,1);\n"
-				"	 _CUMAC1_<<<DimGrid, DimBlock>>>(deviceOutput, deviceA, deviceB, %lu, %lu);\n"
-				"	 cudaDeviceSynchronize();\n"
-				"	 cudaMemcpy(z, deviceOutput, size_z, cudaMemcpyDeviceToHost);\n"
-				"	 cudaFree(deviceA);\n"
-				"	 cudaFree(deviceB);\n"
-				"	 cudaFree(deviceOutput);\n"
-				"  }\n\n",
-				UL(inst->arg[3].i),
-				UL(inst->arg[3].i),
-				UL(inst->arg[4].i))) {
+			"    dim3 DimGrid((%lu+255)/256,1,1);\n"
+			"    dim3 DimBlock(256, 1,1);\n"
+			"    _CUMAC1_<<<DimGrid, DimBlock>>>(deviceOutput, deviceA, deviceB, %lu, %lu);\n"
+			"    cudaDeviceSynchronize();\n"
+			"    cudaMemcpy(z, deviceOutput, size_z, cudaMemcpyDeviceToHost);\n"
+			"    cudaFree(deviceA);\n"
+			"    cudaFree(deviceB);\n"
+			"    cudaFree(deviceOutput);\n"
+			"  }\n\n",
+			UL(inst->arg[3].i),
+			UL(inst->arg[3].i),
+			UL(inst->arg[4].i))) {
 			G__DEBUG(0);
 			return -1;
 		}
@@ -844,7 +844,7 @@ cudaFunction(const struct g__ann *ann, FILE *file)
 				"    for (int j = 0; j < num_input; j++) {\n"
 				"      sum += A[idx * num_input + j] * B[j];\n"
 				"    }\n"
-				"	 z[idx] = sum;"
+				"	 z[idx] = sum;\n"
 				"  }\n"
 				"}\n\n")) {
 			G__DEBUG(0);
