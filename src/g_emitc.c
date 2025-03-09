cae5b4a77b3f6f638f704c140a44aaf174f0818d
Jang Uk (David) Park <jupark814@gmail.com>
2025-03-09 16:07:33 -0700

    ADD

--- src/g_emitc.c
+++ src/g_emitc.c
@@ -408,9 +408,8 @@ static int
 inst_add(const struct g__ann_program_inst *inst, FILE *file)
 {
 	if (inst->cuda_inst) {
-		/*
 		if (P(file,
-			"  {  CUADD \n"
+			"  { /* CUADD */\n"
 			"    %s *za = (%s *)( m_ + %lu );\n"
 			"    const %s *B = (const %s *)( m_ + %lu );\n"
 			"    int size_A = %lu * sizeof(%s);\n"
@@ -447,7 +446,6 @@ inst_add(const struct g__ann_program_inst *inst, FILE *file)
 		  G__DEBUG(0);
 		  return -1;
 		}  
-		*/
 		if (P(file,
 			"  { /* ADD */\n"
 			"    %s *za = (%s *)( m_ + %lu );\n"
